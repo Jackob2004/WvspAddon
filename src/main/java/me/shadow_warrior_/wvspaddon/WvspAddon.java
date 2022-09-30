416e9d699e81bb0a391bbf1e8d67db7c44358847
Kuba <zjakub2004@gmail.com>
2022-09-30 20:56:32 +0200

    Temple updates

--- src/main/java/me/shadow_warrior_/wvspaddon/WvspAddon.java
+++ src/main/java/me/shadow_warrior_/wvspaddon/WvspAddon.java
@@ -1,6 +1,7 @@
 package me.shadow_warrior_.wvspaddon;
 
 import me.shadow_warrior_.wvspaddon.listeners.BlockBreakListener;
+import me.shadow_warrior_.wvspaddon.listeners.BlockClickListener;
 import me.shadow_warrior_.wvspaddon.listeners.PlayerDeathListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,6 +12,7 @@ public final class WvspAddon extends JavaPlugin {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new PlayerDeathListener(),this);
         getServer().getPluginManager().registerEvents(new BlockBreakListener(),this);
+        getServer().getPluginManager().registerEvents(new BlockClickListener(),this);
 
     }
 
---
+++ src/main/java/me/shadow_warrior_/wvspaddon/listeners/TempleHolder.java
@@ -0,0 +1,13 @@
+package me.shadow_warrior_.wvspaddon.listeners;
+
+
+
+import java.util.ArrayList;
+
+public class TempleHolder{
+
+    public ArrayList<Integer> temple2 = new ArrayList<>();
+
+    public ArrayList<Integer> temple = new ArrayList<>();
+
+}
